621c9ae8f2bd0105198e01ebfb95c8f0c6ccec5a
김대희 <0824dh@naver.com>
2021-10-15 10:17:22 +0900

    Initial Commit

---
+++ MiniTown/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniTown
+//
+//  Created by 김대희 on 2021/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
