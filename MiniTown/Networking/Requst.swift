ddc1a25b1c4a0e85a4e9573f01d47147063457ac
김대희 <0824dh@naver.com>
2021-10-16 23:52:19 +0900

    [ADD] request settengs

---
+++ MiniTown/Networking/Requst.swift
@@ -0,0 +1,13 @@
+//
+//  Requst.swift
+//  MiniTown
+//
+//  Created by 김대희 on 2021/10/16.
+//
+
+import Foundation
+import Alamofire
+
+//func postaData(api : Api) -> Data{
+//    AF.request(baseURL + api.path(), method: api.method(), parameters: api.parameter, encoding: URLEncoding.httpBody)
+//}
