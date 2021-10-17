9cdb27948a75599b3a9952efb1ef62ca59314ae0
김대희 <0824dh@naver.com>
2021-10-18 08:20:35 +0900

    [ADD] alert

--- MiniTown/ViewController/LoginViewController.swift
+++ MiniTown/ViewController/LoginViewController.swift
@@ -6,11 +6,12 @@
 //
 
 import UIKit
+import Alamofire
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var idTextField: UITextField!
-    @IBOutlet weak var passWorldTextField: UITextField!
+    @IBOutlet weak var passWordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -20,10 +21,30 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginButton(_ sender: Any) {
+        guard let id = idTextField.text else { return }
+        guard let password = passWordTextField.text  else { return }
+        if id == "" {
+            loginAlert(title: "아이디를 입력하지 않았습니다.")
+        }
+        if password == "" {
+            loginAlert(title: "비밀번호를 입력하지 않았습니다.")
+        } else {
+            print("accessToken : \(String(describing: Token.accessToken))\n refreshToken : \(String(describing: Token.refreshToken))")
+            logindata(id: id, password: password)
+
+        }
+        
     }
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
          self.view.endEditing(true)
    }
+    func loginAlert(title : String) {
+        let alert = UIAlertController(title: "\(title)", message: "다시 입력해 주세요.", preferredStyle: UIAlertController.Style.alert)
+        let defaultAction =  UIAlertAction(title: "ok", style: UIAlertAction.Style.default)
+        alert.addAction(defaultAction)
+        self.present(alert, animated: false)
+    }
 
 }
 
--- MiniTown/ViewController/SignUpViewController.swift
+++ MiniTown/ViewController/SignUpViewController.swift
@@ -8,30 +8,44 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    
     @IBOutlet weak var SidField: UITextField!
-    @IBOutlet weak var SpwField: UITextField!
+    @IBOutlet weak var SnameField: UITextField!
     @IBOutlet weak var SrpwField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     @IBAction func LoginPageButton(_ sender: Any) {
-        
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
-         self.view.endEditing(true)
-   }
-}
+        guard let sidField = SidField.text else { return }
+        guard let snameField = SnameField.text else { return }
+        guard let srpwField = SrpwField.text else { return }
+                
+                if sidField == "" {
+                    alert(title: "아이디가 입력되지 않았습니다.")
+                }
+                if snameField == "" {
+                    alert(title: "이름이 입력되지 않았습니다.")
+                }
+                if srpwField == "" {
+                    alert(title: "비밀번호를 입력하세요.")
+                }
+                else {
+                    signupdata(id: sidField, name: snameField, password: srpwField)
+                    self.navigationController?.popViewController(animated: true)
+                }
+            }
+            
+            override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+                self.view.endEditing(true)
+            }
+            func alert(title : String) {
+                let alert = UIAlertController(title: "\(title)", message: "다시 입력해 주세요.", preferredStyle: UIAlertController.Style.alert)
+                let defaultAction =  UIAlertAction(title: "확인", style: UIAlertAction.Style.default)
+                alert.addAction(defaultAction)
+                self.present(alert, animated: false)
+            }
+        }
