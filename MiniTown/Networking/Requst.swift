d2952e5bd088a07e293d4d40ff762409d3885820
김대희 <0824dh@naver.com>
2021-10-17 23:36:31 +0900

    [ADD] logindata, signupdata

--- MiniTown/Networking/Requst.swift
+++ MiniTown/Networking/Requst.swift
@@ -7,7 +7,70 @@
 
 import Foundation
 import Alamofire
+import UIKit
 
-//func postaData(api : Api) -> Data{
-//    AF.request(baseURL + api.path(), method: api.method(), parameters: api.parameter, encoding: URLEncoding.httpBody)
-//}
+func logindata(id : String, password : String){
+    AF.request(baseURL + Api.login(id, password).path() ,
+               method: Api.login(id, password).method(),
+               parameters: ["id" : id, "password" : password],
+               encoder: JSONParameterEncoder.default)
+        .validate(statusCode: 200..<300)
+    
+        .responseJSON { response in
+            print(response)
+            switch response.result {
+            case .success:
+                print("POST 성공")
+                let decoder = JSONDecoder()
+                guard let data = response.data else { return }
+                do {
+                    if let resault = try? decoder.decode(LoginSuccess.self, from: data) {
+                        if resault.data == nil {
+                            print("")
+                            LoginDone = false
+                            break
+                        }
+                        
+                        Token.accessToken = resault.data?.accessToken
+                        Token.refreshToken = resault.data?.refreshToken
+                        print(resault)
+                        print("----------")
+                        print("accessToken : \(String(describing: Token.accessToken))\n refreshToken : \(String(describing: Token.refreshToken))")
+                    }
+                }
+            case .failure(let error):
+                print("Request Error\nCode:\(error._code), Message: \(error.errorDescription!)")
+            }
+        }
+}
+
+func signupdata(id : String, name : String, password : String){
+    AF.request(baseURL + Api.signup(id, name, password).path() ,
+               method: Api.signup(id, name, password).method(),
+               parameters: ["id" : id, "name" : name, "password" : password],
+               encoder: JSONParameterEncoder.default)
+        .validate(statusCode: 200..<300)
+        .responseJSON { response in
+
+            print(response)
+            switch response.result {
+            case .success:
+                print("POST 성공")
+                let decoder = JSONDecoder()
+                guard let data = response.data else { return }
+                do {
+                    let resault = try decoder.decode(Success.self, from: data)
+                    if resault.data != "success" {
+                    }
+                    print(resault)
+                    
+                }
+                catch {
+                    print(error)
+                }
+            case .failure(let error):
+                print("Request Error\nCode:\(error._code), Message: \(error.errorDescription!)")
+            }
+        }
+}
+
