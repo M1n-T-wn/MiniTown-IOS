12693d955e3ee7c78fdfb870378c12c65c6836cb
김대희 <0824dh@naver.com>
2021-10-20 22:26:17 +0900

    [ADD] SO\OKOT api

--- MiniTown/Networking/Api.swift
+++ MiniTown/Networking/Api.swift
@@ -15,6 +15,14 @@ enum Api {
     case reissue(_ accessToken : String, _ refreshToken : String)
     case signup(_ id : String, _ name : String, _ password : String)
     
+    // Soket
+    
+    case creatChatroom
+    case joinChatroom(_ chatId : Int64)
+    case resignChatroom(_ chatId : Int64)
+    case sendChatroom(_ chatId : Int64)
+    case removeChatroom(_ chatId : Int64)
+
     func path() -> String {
         switch self {
         case .signup:
@@ -23,6 +31,19 @@ enum Api {
             return "/api/auth/login"
         case .reissue:
             return "/api/auth/reissue"
+        case .creatChatroom:
+            return "/websocket/create/chatroom"
+        case .joinChatroom(_):
+            return "/websocket//join/chatroom/{"
+        case .resignChatroom(_):
+            // /websocket/resign/chatroom/{chatRoomId}
+            return "/websocket/resign/chatroom/{"
+        case .sendChatroom(_):
+            // /websocket/chat/{chatRoomId}/send
+            // chatRoomId}/send
+            return "/websocket/chat/{"
+        case .removeChatroom(_):
+            return "/websocket/chat/remove/{"
         }
     }
     func method() -> HTTPMethod {
@@ -33,6 +54,16 @@ enum Api {
             return .post
         case .signup:
             return .post
+        case .creatChatroom:
+            return .get
+        case .joinChatroom(_):
+            return .get
+        case .resignChatroom(_):
+            return .get
+        case .sendChatroom(_):
+            return .get
+        case .removeChatroom(_):
+            return .get
         }
     }
     
