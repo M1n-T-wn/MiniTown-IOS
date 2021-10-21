a9d193420139fd8ceb99854a1c326698ff42b880
김대희 <0824dh@naver.com>
2021-10-21 19:37:31 +0900

    [ADD] New Login Model

--- MiniTown/Model/Model.swift
+++ MiniTown/Model/Model.swift
@@ -10,16 +10,24 @@ import GoogleSignIn
 
 let signInConfig = GIDConfiguration.init(clientID: "105041905321-75o2u6k4v3r7d4t3vg6spucsgnvai8qf.apps.googleusercontent.com")
 
-var LoginDone = true
-var signupDone = true
 
 struct LocalLoginDto : Codable {
     let id : String
     let password : String
 }
 
+struct Sort : Codable {
+    let empty : Bool
+    let sorted : Bool
+    let unsorted : Bool
+}
+
+
 struct LocalSignUpDto : Codable {
     let id : String
+    let gender : String
+    let birth : String
+    let phone : String
     let name : String
     let password : String
 }
@@ -29,11 +37,16 @@ struct Success : Codable {
     let data : String 
 }
 
+
 struct LoginSuccess : Codable {
     let count : Int
     let data : JwtTokenResponse?
 }
-
-struct Chat {
-    let chatId : Int64
+struct LoginSuccess2 : Codable {
+    let count : Int
+    let data : String?
 }
+
+//struct Chat {
+//    let chatId : Int64
+//}
