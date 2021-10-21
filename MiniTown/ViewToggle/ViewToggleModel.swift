a716e914ce74225115ffb0a814555a4b9f44caaf
김대희 <0824dh@naver.com>
2021-10-21 20:16:09 +0900

    [ADD]SignUpViewMOdel

---
+++ MiniTown/ViewToggle/ViewToggleModel.swift
@@ -0,0 +1,33 @@
+//
+//  ViewToggleModel.swift
+//  MiniTown
+//
+//  Created by 김대희 on 2021/10/21.
+//
+
+import Foundation
+
+
+var LoginDone = true
+var signupDone = true
+
+
+var checkId : String?
+var checkPhone : String!
+var checkPhoneD : String?
+
+var certifiedPhone : String?
+
+var checkIdDone = false
+var checkCertifiedDone = true
+// 에러가 고쳐진다면 꼭 고칠 것!!
+
+struct SignUpInfo {
+    var id : String?
+    var phone : String?
+    var birth : String?
+    var gender : String?
+    var password : String?
+}
+var info = SignUpInfo()
+
